0532b9fed281df9662d047c8cb76ee7b703018bd
erickxse <erick_sebastian_@hotmail.com>
2023-08-06 17:43:23 -0500

    PlaylistObtainFix

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,19 @@
-require('dotenv').config();
 // Esperar a que el documento esté completamente cargado
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", async () => {
+    // Obtener las playlists del usuario utilizando la función de app.js
+    const playlists = await APIController.getPlaylists();
+    // Obtener el elemento del DOM donde queremos mostrar las playlists
+    const playlistContainer = document.getElementById("playlist-container");
     // URL de la API de Spotify para obtener las playlists del usuario
     const apiURL = "https://api.spotify.com/v1/me/playlists";
 
+    // Obtener el código de autorización de la URL
+    const urlParams = new URLSearchParams(window.location.search);
+    const code = urlParams.get("code");
+
     // Token de acceso de Spotify (debes obtenerlo mediante el proceso de autorización)
-    const accessToken = process.env.SPOTIFY_ACCESS_TOKEN;
+    const accessToken = "AQCdSMgowlw-IW1CORtB4DQwt83vrt_ZVbOWQksoGPDUYqXgLI_ZpMhjqmkql3ZmebRfoORhtiJxjuhK2wysl9Mqi5Hfmz4mOGoCYTlF1jH-uojz5O0SCesdBBOztHI_ISXhg7lbhwbMvEFyGBA1bOS0Ve8C-fTyxqhGMItBXBobp2buO1pdHzA4Tf0Q8X_0wrml8GquRu1Gfsa2guJFCCQ17vJsCD-ftRjaW4muOXlo0Hs6L_Nk6gZxngs0CXE9_YwjobuFofpKlKEgG4I2KKaw0J_99RAwaVv-QUzpfSP1";
+
 
     // Configuración de la solicitud a la API de Spotify
     const headers = {
