a438675489db4544b3d783ce8484fa3089b1b30d
erickxse <erick_sebastian_@hotmail.com>
2023-08-06 17:47:23 -0500

    Appjs function fix

--- scripts/app.js
+++ scripts/app.js
@@ -1,38 +1,37 @@
-const APIController = (funcion(){
-
+const APIController = (function () {
     const clientId = '970017726a0d4148a54e8887d5985452';
-const clientSecret = '6969761aaa164e60bddd6ef1d49a7f25';
-let accessToken = '';
+    const clientSecret = '6969761aaa164e60bddd6ef1d49a7f25';
+    let accessToken = '';
 
-const _getToken = async () => {
-    const result = await fetch('https://accounts.spotify.com/api/token', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded',
-            'Authorization': 'Basic ' + btoa(clientId + ':' + clientSecret),
-        },
-        body: 'grant_type=client_credentials',
-    });
-    const data = await result.json();
-    accessToken = data.access_token;
-};
-
-const getPlaylists = async () => {
-    if (!accessToken) {
-        await _getToken();
-    }
-
-    const apiURL = 'https://api.spotify.com/v1/me/playlists';
-    const headers = {
-        'Authorization': 'Bearer ' + accessToken,
+    const _getToken = async () => {
+        const result = await fetch('https://accounts.spotify.com/api/token', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'Authorization': 'Basic ' + btoa(clientId + ':' + clientSecret),
+            },
+            body: 'grant_type=client_credentials',
+        });
+        const data = await result.json();
+        accessToken = data.access_token;
     };
 
-    const response = await fetch(apiURL, { headers });
-    const data = await response.json();
-    return data.items;
-};
+    const getPlaylists = async () => {
+        if (!accessToken) {
+            await _getToken();
+        }
 
-return {
-    getPlaylists,
-};
-}) ();
+        const apiURL = 'https://api.spotify.com/v1/me/playlists';
+        const headers = {
+            'Authorization': 'Bearer ' + accessToken,
+        };
+
+        const response = await fetch(apiURL, { headers });
+        const data = await response.json();
+        return data.items;
+    };
+
+    return {
+        getPlaylists,
+    };
+})();
