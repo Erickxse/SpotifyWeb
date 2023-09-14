37fe69b4a5ef4787f52f90e491e79e3eaaca218c
erickxse <erick_sebastian_@hotmail.com>
2023-09-14 13:08:02 -0500

    try token playlist

--- head.js
+++ head.js
@@ -1,48 +1,127 @@
 // Initialize vars
 let access_token = null;
 let user_id = null;
-let songsdisplayed = false;
-let artistsdisplayed = false;
 let playlistdisplayed = false;
 let time_range = 'short_term';
-let time_range_display = 'last 4 weeks';
-let playlist_uris = [];
 let limit = '20';
 
-// Authorization. Key from spotify api website, must send a waypoint through their settings
-// https://developer.spotify.com/documentation/general/guides/authorization-guide/
+// Authorization
 function authorize() {
     const client_id = '970017726a0d4148a54e8887d5985452';
     const redirect_uri = 'https://quiet-rabanadas-52e0a4.netlify.app/login';
     const scopes = 'playlist-read-private playlist-read-collaborative playlist-read';
-  
-  // Store the date
-  const d = new Date();
-  let date = [d.getMonth() +  1, d.getDate(), d.getFullYear()];
-  date = date.join('/');
-  
-    // Create a Token, and finalize it
-    // final result will lead to a humongous URL Link
-    // with the necessary stuff
+
     let url = 'https://accounts.spotify.com/authorize';
     url += '?response_type=token';
     url += '&client_id=' + encodeURIComponent(client_id);
     url += '&scope=' + encodeURIComponent(scopes);
     url += '&redirect_uri=' + encodeURIComponent(redirect_uri);
-    // Paste the final URL format
     window.location = url;
-  }
+}
 
-  // Grab the access token
-// with the help of regex
-// But first do an if
 function getHashValue(key) {
-  if (typeof key !== 'string') {
-    key = '';
-  } else {
-    key = key.toLowerCase();
-  }
-  const keyAndHash = location.hash.match(new RegExp(key + '=([^&]*)'));
-  const value = keyAndHash ? keyAndHash[1] : '';
-  return value;
+    if (typeof key !== 'string') {
+        key = '';
+    } else {
+        key = key.toLowerCase();
+    }
+    const keyAndHash = location.hash.match(new RegExp(key + '=([^&]*)'));
+    const value = keyAndHash ? keyAndHash[1] : '';
+    return value;
+}
+
+function getUserId() {
+    if (access_token) {
+        $.ajax({
+            url: 'https://api.spotify.com/v1/me',
+            headers: {
+                'Authorization': 'Bearer ' + access_token
+            },
+            success: function(response) {
+                user_id = response.id;
+                enableControls();
+            },
+            error: function(jqXHR, textStatus, errorThrown) {
+                handleApiError(jqXHR.status);
+            },
+        });
+    } else {
+        alert('Please log in to Spotify.');
+    }
+}
+
+function getPlaylists() {
+    $('#playlist-button').addClass("loading");
+
+    if (access_token) {
+        $.ajax({
+            url: 'https://api.spotify.com/v1/me/playlists',
+            headers: {
+                'Authorization': 'Bearer ' + access_token,
+            },
+            success: function(response) {
+                $('#playlist-button').removeClass("loading");
+                // Obtén una referencia al contenedor de listas de reproducción
+                const playlistContainer = $('#playlist-container');
+                playlistContainer.empty(); // Limpia cualquier contenido anterior
+
+                if (response.items.length === 0) {
+                    // Si no se encontraron listas de reproducción
+                    playlistContainer.html('<p>No playlists found.</p>');
+                } else {
+                    // Genera el HTML para mostrar las listas de reproducción
+                    let resultsHtml = '';
+                    response.items.forEach((item, i) => {
+                        let playlistName = item.name;
+                        let playlistUrl = item.external_urls.spotify;
+                        let playlistImage = (item.images.length > 0) ? item.images[0].url : 'placeholder-url.jpg';
+
+                        resultsHtml += '<div class="column wide playlist item">';
+                        resultsHtml += '<a href="' + playlistUrl + '" target="_blank"><img src="' + playlistImage + '"></a>';
+                        resultsHtml += '<h4>' + (i + 1) + '. ' + playlistName + '</h4>';
+                        resultsHtml += '</div>';
+                    });
+
+                    // Agrega el HTML generado al contenedor de listas de reproducción
+                    playlistContainer.html(resultsHtml);
+                }
+
+                playlistdisplayed = true;
+            },
+            error: function(jqXHR, textStatus, errorThrown) {
+                handleApiError(jqXHR.status);
+            },
+        });
+    } else {
+        alert('Please log in to Spotify.');
+    }
+}
+
+
+function handleApiError(error) {
+    $('#playlist-button').removeClass("loading");
+    $('#results').html('<p>Error retrieving data from Spotify API. Please try again later.</p>');
+}
+
+$(document).ready(function() {
+    access_token = getHashValue('access_token');
+    
+    $('#playlist-button').on('click', function() {
+        getPlaylists();
+    });
+
+    if (access_token) {
+        getUserId();
+    } else {
+        disableControls();
+    }
+});
+
+function enableControls() {
+    $('#instructions, #login').css('display', 'none');
+    $('#button-segment, #timeForm, #numForm').removeClass("disabled");
+}
+
+function disableControls() {
+    $('#button-segment, #track-button, #artist-button, #timeForm, #numForm').addClass("disabled");
 }
