79805ace7ea591e6e0e8a91c3cd82e1517f12aaa
erickxse <erick_sebastian_@hotmail.com>
2023-09-16 14:09:50 -0500

    Update head.js

--- head.js
+++ head.js
@@ -5,6 +5,7 @@ let playlistdisplayed = false;
 let time_range = 'short_term';
 let currentPage = 1;
 const playlistsPerPage = 4; // Número de listas de reproducción por página
+let playlists = [];
 
 
 // Authorization
